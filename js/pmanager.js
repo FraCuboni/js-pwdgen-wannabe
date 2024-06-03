0680369a0c4e435d6336cba9dd110d4ddd2b3b98
FraCuboni <fracuboni@gmail.com>
2024-06-03 15:22:58 +0200

    let name surname and color done

--- js/pmanager.js
+++ js/pmanager.js
@@ -1,5 +1,14 @@
 // Chiedo il nome
+let name = prompt('Qualè il tuo nome???')
 
 // Chiedo il cognome
+let surname = prompt('Qualè il tuo cognome???')
 
-// Chiedo il numero preferito
+
+// Chiedo il colore
+let color = prompt('Qual è il tuo colore preferito???')
+
+// Aggiungo il numero
+
+
+// Do la nuova password super sicura
